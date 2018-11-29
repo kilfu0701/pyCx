b05970185653bfcdca5f9d6c3ed49fb24f788c43
kilfu0701 <kilfu0701@gmail.com>
2018-11-29 14:54:23 +0900

    change tests folder

--- tests/tests.py
+++ tests/tests.py
@@ -1,31 +1,41 @@
 import os
+import sys
 
-from pyCx import CX, CxQuery
+pyCx_module = os.path.abspath('../')
+sys.path.insert(0, pyCx_module)
+
+try:
+    import pyCx
+    print('pyCx version:', pyCx.__version__)
+except:
+    exit('Error! Cannot import pyCx package.')
+
+
+from pyCx import Cx, CxQuery
 from pyCx import CxFilter as CF
 from pyCx import CxFilterOp as Op
 from pyCx.helpers import date_range
 
 
-
 class CxTest(object):
     def __init__(self):
-        fp = os.path.abspath('env.yaml')
+        test_env = 'env.test.yaml'
+        if not os.path.isfile(file_path):
+            test_env = 'env.yaml'
+
+        fp = os.path.abspath(test_env)
         cache_dir = os.path.abspath('.cache')
 
         self.cx = Cx(cx_config=CxConfig(fp), cache_dir=cache_dir)
         self.cq = self.cx.get_query()
         self.cq.apply_group('zuu')
 
-
     def get_users_traffic(self):
-        csv_file = '.test_users.csv'
+        csv_file = './sample_data/users.csv'
         dates = date_range('2018-10-01', '2018-11-01')
         return self.cq.get_traffic_by_users(csv_file, dates)
 
-
     def test_filters(self):
-        self.cq
-
         pass
 
 
@@ -34,8 +44,4 @@ if __name__ == '__main__':
 
     ct.get_users_traffic()
     ct.test_filters()
-
-
-
-
 
